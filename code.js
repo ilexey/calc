32b5457f9ee816cd4e92f30126d7169613b48417
ilexey <alexeyshitenkov@yandex.ru>
2021-02-27 17:01:29 +0300

    Changing html and js

--- code.js
+++ code.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
-    $('#price, #number').keyup(function() {
-        $('#result').text($('#price').val() * $('#number').val());
+    $('input').keyup(function() {
+        if ($('input').val() >= 0) {
+            $('#result').text($('#price').val() * $('#number').val());
+        } else {
+            alert("Вводите числа больше ноля");
+        }
     });
 });
-/*function calculate(){
-    var price = Number(document.getElementById("price").value);
-    var number = Number(document.getElementById("number").value);
-    var result = document.getElementById("result");
-    var vat = document.getElementById("vat");
-
-    if (price > 0 && number > 0) {
-        if (vat.checked) {
-            var count = price * number * 1.10;
-        } else {
-            var count = price * number;
-        }
-    } else {
-        alert("Вводите числа больше ноля")
-    }
-
-    result.innerHTML = count.toFixed(2);
-    }*/
     
