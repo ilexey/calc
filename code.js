187aedce7b172535c10827914a5ba019ccd579b1
ilexey <alexeyshitenkov@yandex.ru>
2021-03-01 17:23:33 +0300

    Great changes
    In html text type changed to number. In js  alert block remowed and code finished

--- code.js
+++ code.js
@@ -2,12 +2,26 @@ $(document).ready(function() {
     $('h1').click(function() {
         $('.hide').toggle();
     });
-    $('input').keyup(function() {
-        if ($('input').val() >= 0) {
-            $('#result').text($('#price').val() * $('#number').val());
-        } else {
-            alert("Вводите числа больше ноля");
-        }
+
+    $('input').each(function() {
+        $(this).change(function() {            
+            let price = parseFloat($('#price').val());
+            let number = parseFloat($('#number').val());
+            let trademargin = 1 + parseFloat($('#trademargin').val()) / 100;
+
+            let vat = 1;
+            $('#vat:checked').each(function() {
+                vat = 1.1;
+            });
+
+            let result = (price * number * trademargin * vat).toFixed(2);
+
+            if (isNaN(result)) {
+                $('#result').text('0');
+            } else {
+                $('#result').text(result);
+            }
+        });
     });
 });
     
